444f771f5cdfa10ef450304691425cb02563e2d2
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-04-26 15:18:09 -0500

    Added fucntionality
    Madde it work

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 import streamlit as st
+from support import *
 
 st.title("Nexus AI")
 #upload box
 code = st.file_uploader("Upload Code")
 if code is not None:
     if st.button("Analyze"):
-        pass
+        code = extract_code(code)
+        explanation = analyze_file_code(code)
+        st.download_button(
+            label="Download Explanation",
+            data=explanation,
+            file_name="code_analysis.txt",
+            mime="text/plain"
+        )
+        st.subheader("Code Analysis:")
+        st.code(explanation)
+        
