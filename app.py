76b12ce4d3562996a4f9e728622ddbe6e143c0d0
Srinivasa Gudi <239487102+srinivasagudi0@users.noreply.github.com>
2026-04-30 17:08:37 -0500

    Made it look a bit better

--- app.py
+++ app.py
@@ -99,6 +99,16 @@ if mode == "Project Management":
                 st.write(f"**Created At:** {project['created_at']}")
                 st.write(f"**Updated At:** {project['updated_at']}")
                 st.subheader("**Tasks:**")
+                # make it look good by adding apcing
+                st.markdown("<br>", unsafe_allow_html=True)
+                tasks =get_task_by_id(project['id'])
+                try:    
+                    st.write(tasks)
+                except Exception as e:
+                    st.write("No tasks found for this project.")    
+                st.markdown("---")      
+               
+               
                 left, right = st.columns([1, 3])
                 with left:
                     #st.markdown("<br>", unsafe_allow_html=True)
@@ -106,16 +116,9 @@ if mode == "Project Management":
                 with right:
                     #st.markdown("<br>", unsafe_allow_html=True)
                     if st.button("Add +"):
-                        st.info("This feature is under development, will be up as soon as possible.")
                         add_task_dialog(project['id'])
-                # make it look good by adding apcing
-                st.markdown("<br>", unsafe_allow_html=True)
-                tasks =get_task_by_id(project['id'])
-                try:    
-                    st.write(tasks)
-                except Exception as e:
-                    st.write("No tasks found for this project.")            
-                st.markdown("---")
+                  
+                
 
 
 
